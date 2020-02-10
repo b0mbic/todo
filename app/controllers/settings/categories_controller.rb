3ea162e965e06d7136d0f3dde3b87c7ce1b295e3
b0mbic <vlastimil.cernicek@email.cz>
2020-02-10 09:56:40 +0100

    controller refactoring done

--- app/controllers/settings/categories_controller.rb
+++ app/controllers/settings/categories_controller.rb
@@ -1,5 +1,5 @@
 class Settings::CategoriesController < ApplicationController
-  before_action :set_category, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, :set_category, only: [:show, :edit, :update, :destroy]
 
   # GET /categories
   # GET /categories.json
@@ -25,40 +25,31 @@ class Settings::CategoriesController < ApplicationController
   # POST /categories.json
   def create
     @category = Category.new(category_params)
+    @category.user = current_user
 
-    respond_to do |format|
       if @category.save
-        format.html { redirect_to @category, notice: 'Category was successfully created.' }
-        format.json { render :show, status: :created, location: @category }
+        redirect_to [:settings, @category], notice: 'Category was successfully created.'
       else
-        format.html { render :new }
-        format.json { render json: @category.errors, status: :unprocessable_entity }
+        render :new
       end
     end
-  end
 
   # PATCH/PUT /categories/1
   # PATCH/PUT /categories/1.json
   def update
-    respond_to do |format|
       if @category.update(category_params)
-        format.html { redirect_to @category, notice: 'Category was successfully updated.' }
-        format.json { render :show, status: :ok, location: @category }
+        redirect_to [:settings, @category], notice: 'Category was successfully updated.'
       else
-        format.html { render :edit }
-        format.json { render json: @category.errors, status: :unprocessable_entity }
+        render :edit
       end
-    end
   end
 
   # DELETE /categories/1
   # DELETE /categories/1.json
   def destroy
     @category.destroy
-    respond_to do |format|
-      format.html { redirect_to categories_url, notice: 'Category was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+      redirect_to settings_categories_url, notice: 'Category was successfully destroyed.'
+
   end
 
   private
